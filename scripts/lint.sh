1bff81b719be41fa98798e74b8b955eae0899a44
AnsibleGuy <guy@ansibleguy.net>
2023-04-02 10:21:55 +0200

    lint fixes

---
+++ scripts/lint.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+set -e
+
+cd "$(dirname "$0")/.."
+
+echo ''
+echo 'LINTING Python'
+echo ''
+
+pylint --recursive=y .
+
+echo ''
+echo 'LINTING Yaml'
+echo ''
+yamllint .
+
+echo ''
+echo 'LINTING Ansible'
+echo ''
+TMP_COL_DIR='/tmp/ansible_lint/collections'
+mkdir -p "$TMP_COL_DIR/ansible_collections/ansibleguy"
+ln -s "$(pwd)" "$TMP_COL_DIR/ansible_collections/ansibleguy/linuxha"
+ANSIBLE_COLLECTIONS_PATH="$TMP_COL_DIR" ansible-lint -c .ansible-lint.yml
+rm -rf "$TMP_COL_DIR"
+
+echo ''
+echo 'FINISHED LINTING!'
+echo ''
